f361d04442f8d8972baa720b031f7aa400508cee
Meykaye <Meykaye.socials@gmail.com>
2026-01-30 23:52:10 +0530

    [Rishabh] UC5: Refactor hazard calculation and validation into method

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -15,19 +15,9 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
         String machineryState = sc.nextLine();
 
-        if (armPrecision < 0.0 || armPrecision > 1.0) {
-            System.out.println("Error: Arm precision must be 0.0-1.0");
-            return;
-        }
-
-        if (workerDensity < 1 || workerDensity > 20) {
-            System.out.println("Error: Worker density must be 1-20");
-            return;
-        }
-
-        double machineRiskFactor = 3.0;
-        double risk = ((1.0 - armPrecision) * 15.0)
-                + (workerDensity * machineRiskFactor);
+        RobotHazardAuditor auditor = new RobotHazardAuditor();
+        double risk = auditor.CalculateHazardRisk(
+                armPrecision, workerDensity, machineryState);
 
         System.out.println("Robot Hazard Risk Score: " + risk);
     }
